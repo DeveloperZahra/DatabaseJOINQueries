e5fa13b8502e4643172173fb78c829df9e438873
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-05-21 10:59:53 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyDatabaseJOINQueries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
